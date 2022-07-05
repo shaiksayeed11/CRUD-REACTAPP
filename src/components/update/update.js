483d644c6bd1e6853db09a5af0eed10a8189c9d5
shaiksayeed11 <shaiksayeed1106@gmail.com>
2022-07-05 09:22:52 +0530

    react crud

---
+++ src/components/update/update.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { Form, Button } from 'semantic-ui-react';
+import axios from 'axios';
+import { useHistory } from 'react-router';
+export default function Update() {
+    let history = useHistory();
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [ID, setID] = useState(null);
+    const sendDataToAPI = () => {
+        axios.put(`https://62c31da7876c4700f536d056.mockapi.io/crud/${ID}`, {
+            firstName,
+            lastName
+        }).then(() => {
+            history.push('/read')
+        })
+    }
+
+    useEffect(() => {
+        setFirstName(localStorage.getItem('firstName'));
+        setLastName(localStorage.getItem('lastName'));
+        setID(localStorage.getItem('ID'))
+    }, [])
+
+    return (
+        <div>
+            <Form>
+                <Form.Field>
+                    <label>First Name</label>
+                    <input name="fname"
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                        placeholder='First Name' />
+                </Form.Field>
+                <Form.Field>
+                    <label>Last Name</label>
+                    <input
+                        name="lname"
+                        value={lastName}
+                        placeholder='Last Name'
+                        onChange={(e) => setLastName(e.target.value)}
+                    />
+                </Form.Field>
+                <Button type='submit' onClick={sendDataToAPI}>Update</Button>
+            </Form>
+        </div>
+    )
+}
